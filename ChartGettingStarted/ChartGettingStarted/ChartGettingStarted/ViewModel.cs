6ed108b01aa41115fd5702dbede78a6e9b8c4d94
ROOT\divya.venkatesan <divya.venkatesan@syncfusion.com>
2017-08-17 14:52:34 +0530

    no message

---
+++ ChartGettingStarted/ChartGettingStarted/ChartGettingStarted/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Syncfusion.SfChart.XForms;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartGettingStarted
+{
+    public class ViewModel
+    {
+        public ObservableCollection<ChartDataPoint> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<ChartDataPoint>()
+            {
+                 new ChartDataPoint ("Medical", 15.99),
+                 new ChartDataPoint ("Housing", 8),
+                 new ChartDataPoint ("Education", 15.87),
+                 new ChartDataPoint ("Others", 60.14),
+            };
+        }
+    }
+}
